a1d4ff6171f1a27763f66496122688533aa4f5ca
zEEmperon <dan4ik2204.danya@gmail.com>
2022-11-19 13:56:13 +0200

    removed redundant

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import pandas as pd
 import numpy as np
 
 
-def load_csv(filename: str) -> pd.DataFrame:
-    return pd.read_csv(filename)
-
-
 def main():
     # bisection method
     # https://docs.python.org/3/library/bisect.html
@@ -24,14 +20,6 @@ def main():
     X_2 = 15
     X_3 = 14
     X_4 = 13
-
-    # loading .csv file
-    filename = "data.csv"
-    df = load_csv(filename)
-
-    # dividing dataset into X and Y sets
-    X = df['U']
-    Y = df['a']
 
 
 if __name__ == '__main__':
