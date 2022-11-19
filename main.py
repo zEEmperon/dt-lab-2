a5b90e9b868532f4a2295ec04f107d4c3ae2ea1a
zEEmperon <dan4ik2204.danya@gmail.com>
2022-11-19 13:50:39 +0200

    added variant-related constants

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ def main():
     # https://docs.python.org/3/library/bisect.html
     # use scipy optimize algorithms
 
+    # Variant 14
+    Mx = 15
+    My = 25
+    sigma_x = 1
+    sigma_y = 0.5
+    R1 = 0.5
+    R2 = -0.9
+    X_1 = 16
+    X_2 = 15
+    X_3 = 14
+    X_4 = 13
+
     # loading .csv file
     filename = "data.csv"
     df = load_csv(filename)
